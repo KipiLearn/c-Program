cf0d1e117ec902cca5958f033614f0172e483919
KipiLearn <77003776+KipiLearn@users.noreply.github.com>
2021-02-07 15:34:18 +0530

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>>
+int main()
+{
+	char a[10];
+	printf("Enter a alphabet : ");
+	a=getchar();
+	//printf(" \nYour Enter value is : %c\n",a);
+	printf(" \nYour Enter value is : %s\n",a);
+	getch();
+	return 0;
+}
